818c4b0b844dacbb2dd4650c91cbfd48fc6e7d2e
unknown <thiruppathiff007@gmail.com>
2026-04-10 10:43:29 +0800

    update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://chatyou-hxzto0ry1-thiru781s-projects.vercel.app/", // ✅ Replace with your actual Vercel URL
+    origin: "https://chatyou-hxzto0ry1-thiru781s-projects.vercel.app", // ✅ Replace with your actual Vercel URL
     methods: ["GET", "POST"],
   },
 });
